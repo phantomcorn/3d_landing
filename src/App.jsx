cd4f4e93961376c392c8d712a4bc29fd9341d2a0
phantomcorn <jj.phantakorn@outlook.com>
2024-06-30 13:14:04 +0100

    successfully implemented bubble

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SceneInit from './lib/SceneInit'
 import './App.css'
 import { PointerLockControls } from 'three/examples/jsm/Addons.js';
 
+
 function App() {
   const test = new SceneInit();
 
@@ -11,13 +12,31 @@ function App() {
     test.init()
     test.animate()
 
+    const loader = new THREE.CubeTextureLoader()
+    loader.load([
+      'px.png',
+      'nx.png',
+      'py.png',
+      'ny.png',
+      'pz.png',
+      'nz.png'], (texture) => {
 
-    const geo = new THREE.SphereGeometry(1);
-    const map = new THREE.MeshPhongMaterial({color: 0xff0000});
-    const mesh = new THREE.Mesh(geo,map);
+      var skyGeometry = new THREE.BoxGeometry( 2000, 2000, 2000 );
+      var skyMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, envMap: texture, side: THREE.DoubleSide});
+      var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+	    test.scene.add( skyBox );
+    });
+
+
+    
+
+
+    var box = new THREE.BoxGeometry(500,50,500);
+    var mat = new THREE.MeshBasicMaterial({color : 0xff0000, });
+    var mesh = new THREE.Mesh(box,mat);
+    mesh.position.y =- 160;
     test.scene.add(mesh);
-
-  })  
+  }) 
 
 
   return (
--- src/lib/SceneInit.js
+++ src/lib/SceneInit.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-// import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import FresnelShader from "./../shaders/FresnelShader.js"
 
 export default class SceneInit {
     
@@ -13,6 +14,9 @@ export default class SceneInit {
         this.ambientLight = undefined;
         this.directionalLight = undefined;
         this.dlHelper = undefined;
+        
+        this.rSCamera = undefined;
+        this.sphere = undefined;
     }
     
     init() {
@@ -24,26 +28,76 @@ export default class SceneInit {
         this.renderer.shadowMap.enabled = true;
         document.body.appendChild(this.renderer.domElement);
 
-        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
-        this.camera.position.z = 5;
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 20000)
+        this.camera.position.set(0,150,400);
+        this.camera.lookAt(this.scene.position)
 
         window.addEventListener('resize', () => this.onWindowResize(), false);
 
         this.addLight()
-        
-        // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.addBubble()
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     }
 
     addLight() {
         this.ambientLight = new THREE.AmbientLight(0xffffff, Math.PI);
         this.scene.add(this.ambientLight);
-
         this.directionalLight = new THREE.DirectionalLight(0xffffff);
         this.directionalLight.castShadow = true;
-        this.directionalLight.position.y = 50;
+        this.directionalLight.position.y = 10;
         this.dlHelper = new THREE.DirectionalLightHelper(this.directionalLight);
-        this.scene.add(this.directionalLight);
+        // this.scene.add(this.directionalLight);
         // this.scene.add(this.dlHelper);
+
+        // var light = new THREE.PointLight(0xffffff);
+        // light.castShadow = true;
+        // light.position.set(0,250,0);
+        // this.scene.add(light);
+
+    }
+
+    addBubble() {
+        const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(512, {
+            format: THREE.RGBAFormat,
+            generateMipmaps: true,
+            minFilter: THREE.LinearMipmapLinearFilter
+        });
+        this.rSCamera = new THREE.CubeCamera(1, 1000, cubeRenderTarget);
+        this.scene.add(this.rSCamera);
+
+        //---------------------------Bubble-------------------------
+        const geo = new THREE.SphereGeometry(100,64,32);
+        const fShader = FresnelShader;
+
+        const customMaterial = new THREE.ShaderMaterial( 
+        {
+            uniforms: 		THREE.UniformsUtils.clone(fShader.uniforms),
+            vertexShader:   fShader.vertexShader,
+            fragmentShader: fShader.fragmentShader
+        });
+
+        customMaterial.uniforms.tCube.value = this.rSCamera.renderTarget.texture;
+        
+        this.sphere = new THREE.Mesh(geo,customMaterial);
+        this.sphere.receiveShadow = true;
+        this.sphere.castShadow = true;
+        this.rSCamera.position.set(
+            this.sphere.position.x,
+            this.sphere.position.y,
+            this.sphere.position.z
+         )
+        this.scene.add(this.sphere);
+
+        //-------------------------------------------------------------------
+
+        // console.log(this.rSCamera.position);
+        // console.log(this.sphere.position)
+        // // const cube = new THREE.SphereGeometry(100, 32, 32);
+        // // const mat = new THREE.MeshPhongMaterial({envMap: this.rSCamera.renderTarget.texture })
+        // // const cmesh = new THREE.Mesh(cube, mat);
+        // // cmesh.position.y += 50;
+        // // this.scene.add(cmesh);
+        // // cmesh.add(this.rSCamera);
     }
 
     animate() {
@@ -61,6 +115,10 @@ export default class SceneInit {
       render() {
         // NOTE: Update uniform data on each render.
         // this.uniforms.u_time.value += this.clock.getDelta();
+        this.sphere.visible = false;
+        this.rSCamera.update(this.renderer, this.scene );
+        this.sphere.visible = true;
+
         this.renderer.render(this.scene, this.camera);
       }
     
