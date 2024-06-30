ef5543e32168bdcb3bffae14679d8e41512957ee
phantomcorn <jj.phantakorn@outlook.com>
2024-06-30 17:27:08 +0100

    add tile

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import * as THREE from 'three';
 import SceneInit from './lib/SceneInit'
 import './App.css'
 import { PointerLockControls } from 'three/examples/jsm/Addons.js';
+import { texture } from 'three/examples/jsm/nodes/Nodes.js';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
     test.init()
     test.animate()
 
-    const loader = new THREE.CubeTextureLoader()
+    var loader = new THREE.CubeTextureLoader()
     loader.load([
       'px.png',
       'nx.png',
@@ -21,18 +22,27 @@ function App() {
       'pz.png',
       'nz.png'], (texture) => {
 
-      var skyGeometry = new THREE.BoxGeometry( 2000, 2000, 2000 );
-      var skyMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, envMap: texture, side: THREE.DoubleSide});
-      var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
-	    test.scene.add( skyBox );
+      // var skyGeometry = new THREE.BoxGeometry( 2000, 2000, 2000 );
+      // var skyMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, envMap: texture, side: THREE.DoubleSide});
+      // var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+	    // test.scene.add( skyBox );
     });
 
+    var tileLoader = new THREE.TextureLoader();
+    tileLoader.setPath("./tile/")
 
+    var box = new THREE.BoxGeometry(5000,50,5000);
+    var mat = new THREE.MeshStandardMaterial();
+
+    tileLoader.load('normalMap.png', (texture) => mat.normalMap = texture);
+    tileLoader.load('ambientOcclusion.png', (texture) => mat.aoMap = texture)
+    tileLoader.load('baseColor.png', (texture) => mat.map = texture)
+    tileLoader.load('roughness.png', (texture) => mat.roughnessMap = texture)
+    tileLoader.load('height.png', (texture) => mat.displacementMap = texture)
+    // mat.displacementScale = 2.0;
+    // mat.wireframe = true;
     
 
-
-    var box = new THREE.BoxGeometry(500,50,500);
-    var mat = new THREE.MeshBasicMaterial({color : 0xff0000, });
     var mesh = new THREE.Mesh(box,mat);
     mesh.position.y =- 160;
     test.scene.add(mesh);
